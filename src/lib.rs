a5e4cbe8c0d0b51a771096aafb1b46e80b9d4bd4
Jacob Sapoznikow <94275204+RedstoneWizard08@users.noreply.github.com>
2022-06-15 19:41:16 -0700

    Add code

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+pub mod format;
+pub use format::*;
+
+pub async fn get_minecraft_versions() -> MinecraftVersionResult {
+    let client = reqwest::Client::new();
+    let res = client
+        .get("https://launchermeta.mojang.com/mc/game/version_manifest.json")
+        .send()
+        .await;
+
+    let resp = res.unwrap();
+    let text = resp.text().await.unwrap();
+
+    return serde_json::from_str(&text).unwrap();
 }

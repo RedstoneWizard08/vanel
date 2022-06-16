d087947d32be9c00937ce7e50127fb2199640cca
Jacob Sapoznikow <94275204+RedstoneWizard08@users.noreply.github.com>
2022-06-15 19:52:58 -0700

    Fix HTTP requests

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,11 @@
 pub mod format;
+pub mod util;
+
 pub use format::*;
 
 pub async fn get_minecraft_versions() -> MinecraftVersionResult {
-    let client = reqwest::Client::new();
-    let res = client
-        .get("https://launchermeta.mojang.com/mc/game/version_manifest.json")
-        .send()
-        .await;
+    let resp =
+        util::get("https://launchermeta.mojang.com/mc/game/version_manifest.json".to_string());
 
-    let resp = res.unwrap();
-    let text = resp.text().await.unwrap();
-
-    return serde_json::from_str(&text).unwrap();
+    return serde_json::from_str(&resp).unwrap();
 }
